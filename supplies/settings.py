d4b21ea9df0f71f61ac3acb97fbc83116be7be4a
prafful solanki <praffulsolanki00@gmail.com>
2020-09-28 21:00:46 +0530

    Update settings.py

--- supplies/settings.py
+++ supplies/settings.py
@@ -25,13 +25,7 @@ SECRET_KEY = 'b4=ph7!6b$(umn2%i8fy)!84*i7(4+uc^vs*@=y3o5fp@(r#lk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['localhost']
-
 CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOWED_ORIGINS = [
-    "https://23myy.csb.app"
-]
 
 # Application definition
 
@@ -49,9 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
